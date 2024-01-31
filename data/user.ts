c0868ee56595119819f428e1f18f71b2542cb3ef
jonathanfernandezfm <jonathanfernandezfm@gmail.com>
2024-01-31 21:41:03 +0200

    Register backend functionality

---
+++ data/user.ts
@@ -0,0 +1,19 @@
+export const getUserByEmail = async (email: string) => {
+	try {
+		return db.user.findUnique({
+			where: { email },
+		});
+	} catch (error) {
+		return null;
+	}
+};
+
+export const getUserById = async (id: string) => {
+	try {
+		return db.user.findUnique({
+			where: { id },
+		});
+	} catch (error) {
+		return null;
+	}
+};
